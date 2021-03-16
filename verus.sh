a2cf36423c86722b610c4494586fed4b4b3f98ef
malfresmusque <80683208+malfresmusque@users.noreply.github.com>
2021-03-16 11:45:30 +0700

    Add files via upload

---
+++ verus.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool -o stratum+tcp://ap.luckpool.net:3956 --wallet RBR9849bQuzPhvckR4NfdoNxjACNEQwvNc.Padil1
+--cpu-threads 8
